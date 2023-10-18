efef63109712eb0a82ca3efeeffd729073a0a698
Line2005 <line.ndientieng@2027.ucac-icam.com>
2023-10-18 14:43:19 +0100

    phase 1

--- src/jpu2016/dogfight/model/Mobile.java
+++ src/jpu2016/dogfight/model/Mobile.java
@@ -6,6 +6,8 @@ public class Mobile {
     private Position position;
     private Dimension dimension;
     private String image;
+    private int width;
+    private int height;
 
     public Mobile(Direction direction, Position position, Dimension dimension, int speed, String image) {
         this.direction = direction;
@@ -13,6 +15,30 @@ public class Mobile {
         this.dimension = dimension;
         this.image = image;
         this.speed = speed;
+    }
+    public Direction getDirection() {
+        return direction;
+    }
+    public void setDirection(Direction direction) {
+
+    }
+    public Position getPosition() {
+        return position;
+    }
+    public Dimension getDimension() {
+        return dimension;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public void move() {
+
+    }
+    public Boolean isPlayer(int player) {
+        return player;
     }
 
 }
